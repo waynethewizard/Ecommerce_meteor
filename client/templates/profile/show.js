d1bf4a18a7b0c77cd998add4e453d678256f30c0
William Sankey <WSankey@gmail.com>
2015-07-19 10:43:51 -0400

    Update User profile page.

--- client/templates/profile/show.js
+++ client/templates/profile/show.js
@@ -1,10 +1,7 @@
-AutoForm.hooks({
-  profileEdit: {
-    // Called when any operation succeeds, where operation will be
-    // "insert", "update", "submit", or the method name.
-    onSuccess: function(operation, result, template) {
-      FlashMessages.sendSuccess('User profile modified.');
-      Router.go('profileShow',{_id: this.docId});
-    }, 
+Template.profileShow.events({
+  'submit form': function(event) {
+        event.preventDefault();
+        var data = SimpleForm.processForm(event.target);
+	Meteor.users.update(Meteor.userId(), {$set: {profile: data}});
   }
 });
