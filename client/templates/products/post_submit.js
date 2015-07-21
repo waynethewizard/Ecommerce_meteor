2aa4eb95589600d71b35fb119bd7556af1c5bdf9
William Sankey <WSankey@gmail.com>
2015-07-21 00:11:35 -0400

    Artists can now add products.

--- client/templates/products/post_submit.js
+++ client/templates/products/post_submit.js
@@ -1,11 +1,23 @@
+Template.productSubmit.onCreated(function() {
+  Session.set('productSubmitErrors', {});
+});
+
+Template.productSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('productSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('productSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.productSubmit.events({
-	'submit form': function(e) {
-		e.preventDefault();
+  'submit form': function(e) {
+    e.preventDefault();
 
 		var product = {
-			sku: 
-$(e.target).find('[name=sku]').val(),
-			name: $(e.target).find('[name=name]').val(),
+			sku:   $(e.target).find('[name=sku]').val(),
+			name:  $(e.target).find('[name=name]').val(),
 			image: $(e.target).find('[name=image]').val(),
 			style: $(e.target).find('[name=style]').val(),
 			description: $(e.target).find('[name=description]').val(),
@@ -14,7 +26,20 @@ $(e.target).find('[name=sku]').val(),
 			status: $(e.target).find('[name=status]').val()
 		}
 
-		product._id = Products.insert(product);
-		Router.go('productShow', product);
-	}
+    var errors = validateproduct(product);
+    if (errors.title || errors.url)
+      return Session.set('productSubmitErrors', errors);
+    
+    Meteor.call('productInsert', product, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return throwError(error.reason);
+      
+      // show this result but route anyway
+      if (result.productExists)
+        throwError('This link has already been producted');
+      
+      Router.go('productShow', {_id: result._id});  
+    });
+  }
 });
