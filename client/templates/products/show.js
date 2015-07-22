4d79dfaa324df9c37e651348b585e60899f74bc8
William Sankey <WSankey@gmail.com>
2015-07-22 07:16:22 -0400

    Can now add and send items to shopping cart.

--- client/templates/products/show.js
+++ client/templates/products/show.js
@@ -1,8 +1,12 @@
 Template.productShow.events({
-	"click #add-to-cart" : function(ev){
-		ev.preventDefault();
-		addToCart(this.sku, function(err, res){
-			Router.go("cartShow");
-		});
-	}
+  "click #add-to-cart" : function(ev){
+    ev.preventDefault();
+    addToCart(this._id, function(err,res){
+      if(err){
+        console.log(err);
+      }else{
+        Router.go("cartShow");
+      }
+    });
+  }
 });
---
+++ server/publications.js
@@ -0,0 +1,19 @@
+
+Meteor.publish("vendors", function(){
+  return Vendors.find();
+});
+
+Meteor.publish("products-by-vendor", function(slug){
+  check(slug, String);
+  return Products.find({"vendor.slug" : slug})
+});
+
+Meteor.publish("products-by-id", function(_id){
+  check(_id, Match.OneOf(String, undefined));
+  return Products.find({_id : _id});
+});
+
+Meteor.publish("cart", function(key){
+  check(key, String);
+  return Carts.find({userKey : key});
+});
