15d92a8d6358b037036feba82c80b48511e70ba4
William Sankey <WSankey@gmail.com>
2015-08-11 21:19:53 -0400

    Google analytics

--- client/main.js
+++ client/main.js
@@ -24,3 +24,11 @@ UI.registerHelper("markdown", function(text){
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50381576-3', 'auto');
+  ga('send', 'pageview');
