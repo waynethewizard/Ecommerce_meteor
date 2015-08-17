c1d1413374552b512826ba8f91a08571af3f9a86
William Sankey <WSankey@gmail.com>
2015-08-16 22:36:36 -0400

    Attempting to update dashboards.

--- client/templates/profile/show.js
+++ client/templates/profile/show.js
@@ -1,5 +1,8 @@
-Template.profile.helpers({
-	user : function() {
-		return Products.find();
+Template.profile.info = function(){
+		var userId = this.userId;
+		var products = Products.find({userId : userId});
+		var price = products.price;
+	return {
+		price:price
 	}
-});
+};
